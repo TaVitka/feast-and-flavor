fb63333178646516246e09b558aa787a6170328f
TaVitka <tavitka17@gmail.com>
2024-03-07 18:05:45 +0200

    added menu section for menu page

--- src/js/components/_tabs.js
+++ src/js/components/_tabs.js
@@ -2,8 +2,6 @@ const tabsContainer = document.querySelector('.tabs');
 const tabsList = tabsContainer?.querySelector('.tabs__nav');
 const tabButtons = tabsList?.querySelectorAll('a');
 const tabPanels = tabsContainer?.querySelectorAll('.tabs__pane > div');
-
-console.log(tabButtons);
 tabButtons?.forEach((tab, index) => {
   tab.setAttribute('role', 'tab');
   if (index === 0) {
@@ -41,3 +39,44 @@ function switchTab(newTab) {
 
   activePanel.removeAttribute('hidden', false);
 }
+
+
+// switching and changing title for menu on menu page
+const menuSection = document.getElementById('menu');
+const menuTitle = menuSection?.querySelector('h2');
+const menuNav = menuSection?.querySelector('.menu__nav');
+const menuButtons = menuNav?.querySelectorAll('a');
+
+menuButtons?.forEach(( nav,index)=> {
+  if (index === 0) {
+    nav.classList.add('selected');
+  }
+
+  nav.addEventListener('click', function() {
+    let name = nav.innerText;
+    let idThumbTitle = nav.getAttribute("href");
+    let thumbTitle = document.querySelector(`${idThumbTitle}`);
+    let thumbItem = thumbTitle?.nextElementSibling.querySelector('a');
+    let thumbItemClass = thumbItem?.classList.item(0);
+    let allThumbItems = document.querySelectorAll(`.${thumbItemClass}`);
+
+
+    // changing content inside <h2>
+    menuTitle.innerText = name;
+
+    // making clicked nav link active
+    menuButtons.forEach(button => {
+      button.classList.remove('selected');
+    });
+    nav.classList.add('selected');
+
+    // moving and making active first menu tab in right menu chapter
+    allThumbItems.forEach(button => {
+      button.classList.remove('selected');
+    });
+    thumbItem.classList.add('selected');
+    switchTab(thumbItem);
+
+  });
+})
+
